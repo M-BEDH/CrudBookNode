f42eef2e8eddf0b52fb8b22d3ff4dbea43eca60e
M-BEDH <mel.b.ms@pm.me>
2025-10-02 15:44:48 +0200

    Update -> correction pour format input utilisation de  new Date(data.parution).toISOString().split("T")[0];

--- frontend/src/components/Book.js
+++ frontend/src/components/Book.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import dayjs from "dayjs"
+import dayjs from "dayjs";
+
 
 function Books () {
      const [book, setBook] = useState([]);
@@ -19,39 +20,48 @@ function Books () {
       });
   }, []); 
 
+  const handleDelete = async (id) => {
+    try {
+  
+      await axios.delete(`http://localhost:8081/book/${id}`);
+  
+      window.location.reload();
+    } catch (error) {
+  
+      console.error(error);
+      alert("Erreur lors de la suppression de l'étudiant");
+    }
+  };
 
 
   return (
     <div className="d-flex justify-content-center align-items-center vh-100">
-      <div className="w-50 border bg-white p-4 rounded">
-        <Link to="create" className="btn btn-outline-success mb-2">
-          Ajouter un livre
-        </Link>
-        <table className="table">
+      <div className="w-75 border bg-white pt-3 rounded">
+        <table className="table table-striped">
           <thead>
             <tr>
               <th>Titre</th>
               <th>Auteur</th>
               <th>Parution</th>
-              <th>Action</th>
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
-            {book.map((data) => (
-              <tr key={data.index}>
+            {book.map((data, index) => (
+              <tr key={index}>
                 <td>{data.title}</td>
                 <td>{data.autor}</td>
-                <td>{dayjs(data.parution).format("MM-YYYY")} </td>
+                <td>{dayjs(data.parution).format("YYYY")} </td>
                 <td>
                   <Link
                    to={`/update/${data.id}`}
-                   className="btn btn-primary m-1"
+                   className="btn btn-outline-primary m-1"
                   >
                     Modifier
                   </Link>
                   <button
                      className="btn btn-danger m-1"
-                    // onClick={() => handleDelete(data.id)}
+                    onClick={() => handleDelete(data.id)}
                   >
                     Supprimer
                   </button>
@@ -60,6 +70,9 @@ function Books () {
             ))}
           </tbody>
         </table>
+        <Link to="create" className="btn btn-primary mb-2">
+          Ajouter un livre
+        </Link>
       </div>
     </div>
   );
--- frontend/src/components/UpdateBook.js
+++ frontend/src/components/UpdateBook.js
@@ -2,90 +2,96 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
-
 function UpdateBook() {
     const [title, setTitle] = useState("")
     const [autor, setAutor] = useState("")
     const [parution, setParution] = useState("")
 
-    const {id} = useParams();
+    const { id } = useParams();
     const navigate = useNavigate();
 
+ useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const res = await axios.get(`http://localhost:8081/book/${id}`);
+            const data = res.data;
 
-     useEffect(() => {
- 
-    axios
-      .get(`http://localhost:8081/book/${id}`)
-      .then((res) => {
-   
-        setTitle(res.data.title);
-        setAutor(res.data.autor);
-        setParution(res.data.parution);
-      })
-      .catch((error) => {
- 
-        console.error(error);
-     
-        navigate("/");
-      });
-  }, [id, navigate]); 
+            setTitle(data.title);
+            setAutor(data.autor);
+
+        
+            if (data.parution) {
+                const formattedDate = new Date(data.parution).toISOString().split("T")[0];
+                setParution(formattedDate);
+            } else {
+                setParution("");
+            }
+        } catch (error) {
+            navigate("/");
+        }};
+
+    fetchData();
+}, [id, navigate]);
 
 
-    
     function handleSubmit(event) {
         event.preventDefault();
 
-        axios.get("http://localhost:8081/update", { title, autor, parution })
-        .then((res) => {
-            navigate("/")
-        })
-        .catch((error) => {
-        console.error("Erreur:", error);
-      });
+        const updateBook = { title, autor, parution };
+
+        axios.put(`http://localhost:8081/update/${id}`, updateBook)
+            .then((res) => {
+                console.log("Livre modifié:", res);
+                navigate("/")
+            })
+            .catch((error) => {
+                console.error("Erreur:", error);
+            });
     }
 
-  return (
-    <div className="d-flex bg-primary justify-content-center align-items-center vh-100">
-      <div className="bg-white p-3 rounded w-50 ">
-        <form onSubmit={handleSubmit}>
-          <h2>Modifier le livre</h2>
-          <div className="mb-2">
-            <label htmlFor="title">Titre</label>
-            <input
-              type="text"
-              placeholder="Entrer le titre"
-              className="form-control"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </div>
 
-          <div className="mb-2">
-            <label htmlFor="autor">Auteur</label>
-            <input
-              type="text"
-              placeholder="Entrer l'auteur"
-              className="form-control"
-              value={autor}
-              onChange={(e) => setAutor(e.target.value)}
-            />
-          </div>
-           <div className="mb-2">
-            <label htmlFor="parution">Parution</label>
-            <input
-              type="date"
-              className="form-control"
-              value={parution} 
-              onChange={(e) => setParution(e.target.value)}
-            />
-          </div>
-          <button className="btn btn-success w-10">
-            Modifier
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+    return (
+        <div className="d-flex bg-primary justify-content-center align-items-center vh-100">
+            <div className="bg-white rounded w-50 p-4 m-2">
+                <form onSubmit={handleSubmit}>
+                    <h2>Modifier le livre</h2>
+                    <div className="mb-2">
+                        <label htmlFor="title">Titre</label>
+                        <input
+                            type="text"
+                            placeholder="Entrer le titre"
+                            className="form-control"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="mb-2">
+                        <label htmlFor="autor">Auteur</label>
+                        <input
+                            type="text"
+                            placeholder="Entrer l'auteur"
+                            className="form-control"
+                            value={autor}
+                            onChange={(e) => setAutor(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="parution">Parution</label>
+                        <input
+                            type="date"
+                            className="form-control"
+                            value={parution}
+                            onChange={(e) => setParution(e.target.value)}
+                        />
+                    </div>
+                    <button className="btn btn-success w-10">
+                        Modifier
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 
